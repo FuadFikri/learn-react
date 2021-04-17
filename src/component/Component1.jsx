c83adedbeb1eeaf8195c07b4465bf14094386b9a
Fikri <fuad.syamsudin@ist.id>
2021-04-17 12:20:26 +0700

    component

---
+++ src/component/Component1.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Component3 from './Component3';
+
+
+class Component1 extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Ini statefull component</h1>
+
+                <Component3/>
+            </div>
+        )
+    }
+}
+
+export default Component1;
---
+++ src/component/Component3.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Component3 = () => {
+    return <h1>ini component 3</h1>
+}
+export default Component3;
