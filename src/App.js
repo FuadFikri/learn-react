f0732807d1e7f4f4c517c09b30d1a7bbf29b7bc0
Fikri <fuad.syamsudin@ist.id>
2021-04-17 13:04:53 +0700

    props pada stateless component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Biodata from './component/Biodata';
 import Component1 from './component/Component1';
 import Component2 from './component/Component2';
+import NewBiodata from './component/NewBiodata';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
 <Biodata nama="fikri" semester="3" hobi={<Hobi/>} />
 <Biodata nama="isnaini" semester="5"/>
 
+
+<NewBiodata name="Fuad" />
     </div>
    
   );
---
+++ src/component/NewBiodata.js
@@ -0,0 +1,18 @@
+import { React } from "react";
+
+
+const NewBiodata = (props) => {
+    return(
+        <div>
+            <p>new biodata</p>
+        <h3>Nama : {props.nama}</h3>
+        <p>Semester :  {props.semester} </p>
+        <div>
+            {props.hobi}
+        </div>
+        <p>=========================================</p>
+    </div>
+    )
+}
+
+export default NewBiodata;
